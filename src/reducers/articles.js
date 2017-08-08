092f4c4b5701e8b373967fb7550c4bcef5cbc1d8
Unknown <jorgedgm11@gmail.com>
2017-08-08 09:00:49 +0200

    CRUD actions and tests have been added

--- src/reducers/articles.js
+++ src/reducers/articles.js
@@ -1,5 +1,5 @@
 import { FETCH_ARTICLES } from '../actions/articles';
-import { FETCH_ARTICLE } from '../actions/article';
+import { CREATE_ARTICLE, FETCH_ARTICLE, UPDATE_ARTICLE, REMOVE_ARTICLE } from '../actions/article';
 
 const initialState = {
   articlesById: {},
@@ -7,23 +7,60 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-  if (action.type === FETCH_ARTICLES) {
-    return {
-      articlesById: action.payload.reduce(
+  switch (action.type) {
+    case FETCH_ARTICLES: {
+      return {
+        articlesById: action.payload.reduce(
+          (articlesById, article) => ({ ...articlesById, [article.id]: article }), {},
+        ),
+        articleDetailsById: state.articleDetailsById,
+      };
+    }
+    case FETCH_ARTICLE: {
+      const articleDetailsById = action.payload.reduce(
         (articlesById, article) => ({ ...articlesById, [article.id]: article }), {},
-      ),
-      articleDetailsById: state.articleDetailsById,
-    };
-  } else if (action.type === FETCH_ARTICLE) {
-    const articleDetailsById = action.payload.reduce(
-      (articlesById, article) => ({ ...articlesById, [article.id]: article }), {},
-    );
-    const articles = state.articlesById;
-    return {
-      articlesById: { ...articles, ...articleDetailsById },
-      articleDetailsById,
-    };
+      );
+      const articles = state.articlesById;
+      return {
+        articlesById: { ...articles, ...articleDetailsById },
+        articleDetailsById,
+      };
+    }
+    case CREATE_ARTICLE: {
+      return {
+        articlesById: {
+          ...state.articlesById,
+          [action.payload.id]: action.payload,
+        },
+        articleDetailsById: state.articleDetailsById,
+      };
+    }
+    case UPDATE_ARTICLE: {
+      return {
+        articlesById: {
+          ...state.articlesById,
+          [action.payload.id]: action.payload,
+        },
+        articleDetailsById: { [action.payload.id]: action.payload },
+      };
+    }
+    case REMOVE_ARTICLE: {
+      const articlesById = Object.keys(state.articlesById)
+        .reduce((result, key) => {
+          if (key !== action.id) {
+            return { [key]: state.articlesById[key] };
+          }
+          return result;
+        }, {});
+      return {
+        articlesById: {
+          ...articlesById,
+        },
+        articleDetailsById: state.articleDetailsById,
+      };
+    }
+    default: {
+      return state;
+    }
   }
-  return state;
 };
-
--- src/reducers/articles.test.js
+++ src/reducers/articles.test.js
@@ -1,5 +1,5 @@
 import reducer from './articles';
-import { FETCH_ARTICLE } from '../actions/article';
+import { FETCH_ARTICLE, CREATE_ARTICLE, UPDATE_ARTICLE, REMOVE_ARTICLE } from '../actions/article';
 import { FETCH_ARTICLES } from '../actions/articles';
 
 const initialState = {
@@ -7,14 +7,25 @@ const initialState = {
   articleDetailsById: {},
 };
 
-const article = [{
+const article = {
   author: 'Robb Farrell',
   content: 'ex molestias et. Harum placeat soluta officiis fuga.↵',
   id: '5467',
   published: false,
   tags: ['convergence', 'Run plum exploit'],
   title: 'Corporate Identity Director',
-}];
+};
+
+const articleById = {
+  5467: {
+    author: 'Robb Farrell',
+    content: 'ex molestias et. Harum placeat soluta officiis fuga.↵',
+    id: '5467',
+    published: false,
+    tags: ['convergence', 'Run plum exploit'],
+    title: 'Corporate Identity Director',
+  },
+};
 
 const articles = [{
   author: 'Robb Farrell',
@@ -51,29 +62,6 @@ const articlesById = {
   },
 };
 
-const articleDetailsById = {
-  articleDetailsById: {
-    5467: {
-      author: 'Robb Farrell',
-      content: 'ex molestias et. Harum placeat soluta officiis fuga.↵',
-      id: '5467',
-      published: false,
-      tags: ['convergence', 'Run plum exploit'],
-      title: 'Corporate Identity Director',
-    },
-  },
-  articlesById: {
-    5467: {
-      author: 'Robb Farrell',
-      content: 'ex molestias et. Harum placeat soluta officiis fuga.↵',
-      id: '5467',
-      published: false,
-      tags: ['convergence', 'Run plum exploit'],
-      title: 'Corporate Identity Director',
-    },
-  },
-};
-
 describe('articles reducer', () => {
   it('should handle initial state', () => {
     expect(reducer(initialState, {})).toEqual(initialState);
@@ -89,7 +77,70 @@ describe('articles reducer', () => {
   it('should add article to articleDetailsById', () => {
     expect(reducer(initialState, {
       type: FETCH_ARTICLE,
+      payload: [article],
+    })).toEqual({
+      articleDetailsById: articleById,
+      articlesById: articleById,
+    });
+  });
+
+  it('should create an article', () => {
+    expect(reducer(initialState, {
+      type: CREATE_ARTICLE,
       payload: article,
-    })).toEqual(articleDetailsById);
+    })).toEqual({
+      articleDetailsById: {},
+      articlesById: articleById,
+    });
+  });
+
+  it('should update an article', () => {
+    expect(reducer({
+      articleDetailsById: articleById,
+      articlesById: articleById,
+    }, {
+      type: UPDATE_ARTICLE,
+      payload: {
+          author: 'Robb Stark',
+          content: 'ex molestias et. Harum placeat soluta officiis fuga.↵',
+          id: '5467',
+          published: true,
+          tags: ['convergence', 'Run plum exploit'],
+          title: 'Corporate Identity Director',
+        },
+    })).toEqual({
+        articleDetailsById: {
+          5467: {
+            author: 'Robb Stark',
+            content: 'ex molestias et. Harum placeat soluta officiis fuga.↵',
+            id: '5467',
+            published: true,
+            tags: ['convergence', 'Run plum exploit'],
+            title: 'Corporate Identity Director',
+          },
+        },
+        articlesById: {
+          5467: {
+            author: 'Robb Stark',
+            content: 'ex molestias et. Harum placeat soluta officiis fuga.↵',
+            id: '5467',
+            published: true,
+            tags: ['convergence', 'Run plum exploit'],
+            title: 'Corporate Identity Director',
+          },
+        },
+      });
+  });
+  it('should remove an article', () => {
+    expect(reducer({
+      articleDetailsById: {},
+      articlesById: articleById,
+    }, {
+      type: REMOVE_ARTICLE,
+      id: article.id,
+    })).toEqual({
+        articleDetailsById: {},
+        articlesById: {},
+      });
   });
 });
