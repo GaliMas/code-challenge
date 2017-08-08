b69b3c6583d83eff86023abe0d3558827d9033d4
Unknown <jorgedgm11@gmail.com>
2017-08-08 09:04:57 +0200

    added create button and modal

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { CardList } from '../../components/Card';
+import CreateArticleForm from '../CreateArticleForm';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.onCreateArticle = this.onCreateArticle.bind(this);
+    this.toogleModal = this.toogleModal.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchArticles();
   }
 
+  onCreateArticle(article) {
+    this.props.createArticle(article);
+    this.toogleModal();
+  }
+
+  toogleModal() {
+    this.setState(prevState => ({ isModalOpen: !prevState.isModalOpen }));
+  }
+
   render() {
     return (
-      <CardList cards={this.props.articles} />
+      <div>
+        <button
+          className="btn btnAdd"
+          onClick={this.toogleModal}
+        >+</button>
+        <CardList cards={this.props.articles} />
+        {this.state.isModalOpen &&
+          <CreateArticleForm
+            onClose={this.toogleModal}
+            onSave={this.onCreateArticle}
+            onFieldChange={this.handleChange}
+          />
+        }
+      </div>
     );
   }
 }
@@ -20,7 +51,8 @@ Home.propTypes = {
     author: PropTypes.string,
     content: PropTypes.string,
   })),
-  fetchArticles: PropTypes.func,
+  createArticle: PropTypes.func.isRequired,
+  fetchArticles: PropTypes.func.isRequired,
 };
 
 export default Home;
