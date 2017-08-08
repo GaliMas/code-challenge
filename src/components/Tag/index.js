9fc4a579d36d36bd9a1b4d6d131a69bc4ab8614a
Unknown <jorgedgm11@gmail.com>
2017-08-08 08:59:53 +0200

    improve tag styles and jsx

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './index.css';
 
-const Tag = ({ children }) => <span className="tag">{children}</span>;
+const Tag = ({ children }) => <span className="tag"><span className="tagText">{children}</span></span>;
 
 Tag.propTypes = {
   children: PropTypes.node,
